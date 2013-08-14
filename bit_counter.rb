f8ba4acacbfdc199ad5f4cbca15ebb5418704787
Jamie White <jamie@jgwhite.co.uk>
2013-08-14 08:02:30 +0100

    Make bit counter aware of zero

--- bit_counter.rb
+++ bit_counter.rb
@@ -13,12 +13,15 @@ class Tape
 end
 
 MACHINE_RULEBOOK = DTMRulebook.new([
+  TMRule.new(0, '0', 0, '0', :right),
+  TMRule.new(0, '1', 1, '1', :right),
+  TMRule.new(0, '_', 7, '_', :left),
   TMRule.new(1, '0', 1, '0', :right),
   TMRule.new(1, '1', 1, '1', :right),
   TMRule.new(1, '_', 2, '_', :left),
   TMRule.new(2, '0', 3, '_', :left),
   TMRule.new(2, '1', 3, '_', :left),
-  TMRule.new(2, '_', 9, '_', :left),
+  TMRule.new(2, '_', 7, '_', :left),
   TMRule.new(3, '0', 3, '0', :left),
   TMRule.new(3, '1', 3, '1', :left),
   TMRule.new(3, '_', 4, '_', :left),
@@ -37,8 +40,8 @@ class TestMachine < MiniTest::Unit::TestCase
   def build_machine(n)
     head, *rest = n.to_s(2).chars
     tape = Tape.new([], head, rest, '_')
-    configuration = TMConfiguration.new(1, tape)
-    accept_states = []
+    configuration = TMConfiguration.new(0, tape)
+    accept_states = [7]
     DTM.new(configuration, accept_states, MACHINE_RULEBOOK)
   end
 
@@ -49,48 +52,45 @@ class TestMachine < MiniTest::Unit::TestCase
   end
 
   def test_machine
-    assert_equal 1, run_machine(0) # => true
-    assert_equal 1, run_machine(1) # => true
-    assert_equal 2, run_machine(2) # => true
-    assert_equal 2, run_machine(3) # => true
-    assert_equal 3, run_machine(4) # => true
-    assert_equal 3, run_machine(5) # => true
-    assert_equal 3, run_machine(6) # => true
-    assert_equal 3, run_machine(7) # => true
-    assert_equal 4, run_machine(8) # => true
-    assert_equal 4, run_machine(9) # => true
-    assert_equal 4, run_machine(10) # => true
-    assert_equal 4, run_machine(11) # => true
-    assert_equal 4, run_machine(12) # => true
-    assert_equal 4, run_machine(13) # => true
-    assert_equal 4, run_machine(14) # => true
-    assert_equal 4, run_machine(15) # => true
-    assert_equal 5, run_machine(16) # => true
-    assert_equal 5, run_machine(17) # => true
-    assert_equal 5, run_machine(18) # => true
-    assert_equal 5, run_machine(19) # => true
-    assert_equal 5, run_machine(20) # => true
-    assert_equal 5, run_machine(21) # => true
-    assert_equal 5, run_machine(22) # => true
-    assert_equal 5, run_machine(23) # => true
-    assert_equal 5, run_machine(24) # => true
-    assert_equal 5, run_machine(25) # => true
-    assert_equal 5, run_machine(26) # => true
-    assert_equal 5, run_machine(27) # => true
-    assert_equal 5, run_machine(28) # => true
-    assert_equal 5, run_machine(29) # => true
-    assert_equal 5, run_machine(30) # => true
-    assert_equal 5, run_machine(31) # => true
-    assert_equal 6, run_machine(32) # => true
+    assert_equal 0, run_machine(0), "k of 0 should be 0"
+    assert_equal 1, run_machine(1), "k of 1 should be 1"
+
+    (2..3).each do |n|
+      assert_equal 2, run_machine(n), "k of #{n} should be 2"
+    end
+
+    (4..7).each do |n|
+      assert_equal 3, run_machine(n), "k of #{n} should be 3"
+    end
+
+    (8..15).each do |n|
+      assert_equal 4, run_machine(n), "k of #{n} should be 4"
+    end
+
+    (16..31).each do |n|
+      assert_equal 5, run_machine(n), "k of #{n} should be 5"
+    end
+
+    (32..63).each do |n|
+      assert_equal 6, run_machine(n), "k of #{n} should be 6"
+    end
+
+    (64..127).each do |n|
+      assert_equal 7, run_machine(n), "k of #{n} should be 7"
+    end
+
+    (128..255).each do |n|
+      assert_equal 8, run_machine(n), "k of #{n} should be 8"
+    end
   end
 end
 
-# >> Run options: --seed 24618
+# >> Run options: --seed 51261
 # >> 
 # >> # Running tests:
 # >> 
 # >> .
 # >> 
-# >> Finished tests in 0.025067s, 39.8931 tests/s, 1316.4719 assertions/s.
+# >> Finished tests in 0.399200s, 2.5050 tests/s, 641.2826 assertions/s.
 # >> 
-# >> 1 tests, 33 assertions, 0 failures, 0 errors, 0 skips
+# >> 1 tests, 256 assertions, 0 failures, 0 errors, 0 skips
