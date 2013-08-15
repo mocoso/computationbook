3252d43158615ed4716ed09c4cef196bc24057b9
Joel Chippindale <joel@joelchippindale.com>
2013-08-15 08:00:30 +0100

    Remove rules that put machine into an accept state
    Because it is enough that this machine becomes 'stuck'

--- bit_counter.rb
+++ bit_counter.rb
@@ -1,8 +1,7 @@
 # The machine is assumed to start on the most significant
-# bit of the input number n. It will work until it reaches
-# its terminating accept state 7, at which point the
-# tape head should be on the least significant bit
-# of the output result k.
+# bit of the input number n. It will work until it gets stuck,
+# at which point the tape head should be on the least significant
+# bit of the output result k.
 #
 # Encoding is just numbers in binary.
 
@@ -23,13 +22,11 @@ end
 MACHINE_RULEBOOK = DTMRulebook.new([
   TMRule.new(0, '0', 0, '0', :right),
   TMRule.new(0, '1', 1, '1', :right),
-  TMRule.new(0, '_', 7, '_', :left),
   TMRule.new(1, '0', 1, '0', :right),
   TMRule.new(1, '1', 1, '1', :right),
   TMRule.new(1, '_', 2, '_', :left),
   TMRule.new(2, '0', 3, '_', :left),
   TMRule.new(2, '1', 3, '_', :left),
-  TMRule.new(2, '_', 7, '_', :left),
   TMRule.new(3, '0', 3, '0', :left),
   TMRule.new(3, '1', 3, '1', :left),
   TMRule.new(3, '_', 4, '_', :left),
@@ -47,7 +44,7 @@ MACHINE_RULEBOOK = DTMRulebook.new([
 class TestMachine < MiniTest::Unit::TestCase
   def build_machine(n)
     configuration = TMConfiguration.new(0, input_tape(n))
-    accept_states = [7]
+    accept_states = []
     DTM.new(configuration, accept_states, MACHINE_RULEBOOK)
   end
 
