3f6520a85130d01fd7ffc80981bc09aeb4f7c262
Joel Chippindale <joel@joelchippindale.com>
2013-09-01 22:25:27 +0100

    Remove unused booleans

--- programming_with_nothing/fibonacci.rb
+++ programming_with_nothing/fibonacci.rb
@@ -3,10 +3,6 @@ def to_integer(proc)
   proc[-> n { n + 1 }][0]
 end
 
-def to_boolean(proc)
-  proc[true][false]
-end
-
 # Some numbers
 ZERO    = -> p { -> x {             x      } }
 ONE     = -> p { -> x {           p[x]     } }
@@ -14,9 +10,6 @@ TWO     = -> p { -> x {         p[p[x]]    } }
 THREE   = -> p { -> x {       p[p[p[x]]]   } }
 FOUR    = -> p { -> x {     p[p[p[p[x]]]]  } }
 FIVE    = -> p { -> x {   p[p[p[p[p[x]]]]] } }
-
-TRUE    = -> x { -> y { x } }
-FALSE   = -> x { -> y { y } }
 
 INCREMENT = -> n { -> p { -> x { n[p][p[x]] } } }
 
@@ -47,12 +40,6 @@ describe 'to_integer' do
   it { to_integer(ONE).should == 1 }
   it { to_integer(TWO).should == 2 }
 end
-
-describe 'to_boolean' do
-  it { to_boolean(TRUE).should == true }
-  it { to_boolean(FALSE).should == false }
-end
-
 
 describe 'LEFT' do 
   it { to_integer(LEFT[PAIR[ZERO][ONE]]).should == 0 }
