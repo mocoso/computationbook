f7e533c0064436543f669ac9540ba94a2607abf3
Joel Chippindale <joel@joelchippindale.com>
2013-09-01 22:15:44 +0100

    Switch to iterative definition of FIB

--- programming_with_nothing/fibonacci.rb
+++ programming_with_nothing/fibonacci.rb
@@ -8,9 +8,12 @@ def to_boolean(proc)
 end
 
 # Some numbers
-ZERO    = -> p { -> x {       x    } }
-ONE     = -> p { -> x {     p[x]   } }
-TWO     = -> p { -> x {   p[p[x]]  } }
+ZERO    = -> p { -> x {             x      } }
+ONE     = -> p { -> x {           p[x]     } }
+TWO     = -> p { -> x {         p[p[x]]    } }
+THREE   = -> p { -> x {       p[p[p[x]]]   } }
+FOUR    = -> p { -> x {     p[p[p[p[x]]]]  } }
+FIVE    = -> p { -> x {   p[p[p[p[p[x]]]]] } }
 
 TRUE    = -> x { -> y { x } }
 FALSE   = -> x { -> y { y } }
@@ -36,10 +39,18 @@ ADD = -> a { -> b { a[INCREMENT][b] } }
 # end
 
 FIB = -> n {
-  EQUALS_ZERO[n][
-    ONE
-  ][
-    ONE
+  LEFT[
+    n[
+      -> p {
+        PAIR[
+          RIGHT[p]
+        ][
+          ADD[LEFT[p]][RIGHT[p]]
+        ]
+      }
+    ][
+      PAIR[ONE][ONE]
+    ]
   ]
 }
 
@@ -78,7 +89,10 @@ end
 describe 'FIB' do
   it { to_integer(FIB[ZERO]).should == 1 }
   it { to_integer(FIB[ONE]).should == 1 }
-  xit { to_integer(FIB[TWO]).should == 2 }
+  it { to_integer(FIB[TWO]).should == 2 }
+  it { to_integer(FIB[THREE]).should == 3 }
+  it { to_integer(FIB[FOUR]).should == 5 }
+  it { to_integer(FIB[FIVE]).should == 8 }
 end
 
 describe 'INCREMENT' do
